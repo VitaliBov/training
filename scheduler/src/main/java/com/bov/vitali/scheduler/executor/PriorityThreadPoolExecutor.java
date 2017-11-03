68b4fa0e4461d7a81a0e6990020d76ca2805ccf9
vitali <vitalibof@gmail.com>
2017-11-03 12:47:34 +0300

    The submit methods from PriorityThreadPoolExecutor have been removed

--- scheduler/src/main/java/com/bov/vitali/scheduler/executor/PriorityThreadPoolExecutor.java
+++ scheduler/src/main/java/com/bov/vitali/scheduler/executor/PriorityThreadPoolExecutor.java
@@ -1,7 +1,5 @@
 package com.bov.vitali.scheduler.executor;
 
-import android.support.annotation.NonNull;
-
 import com.bov.vitali.scheduler.core.PriorityCallable;
 import com.bov.vitali.scheduler.core.PriorityCallableFutureTask;
 import com.bov.vitali.scheduler.core.PriorityRunnable;
@@ -9,7 +7,6 @@ import com.bov.vitali.scheduler.core.PriorityRunnableFutureTask;
 
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.Callable;
-import java.util.concurrent.Future;
 import java.util.concurrent.RunnableFuture;
 import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.ThreadPoolExecutor;
@@ -34,49 +31,21 @@ public class PriorityThreadPoolExecutor extends ThreadPoolExecutor {
         super(corePoolSize, maximumPoolSize, keepAliveTime, unit, queue);
     }
 
-    @NonNull
-    @Override
-    public Future<?> submit(Runnable task) {
-        if (task instanceof PriorityRunnable) {
-            RunnableFuture<?> futureTask = newTaskFor(task, null);
-            execute(futureTask);
-            return futureTask;
-        } else {
-            return super.submit(task);
-        }
-    }
-
-    @NonNull
-    @Override
-    public <T> Future<T> submit(Runnable task, T result) {
-        if (task instanceof PriorityRunnable) {
-            RunnableFuture<T> futureTask = newTaskFor(task, result);
-            execute(futureTask);
-            return futureTask;
-        } else {
-            return super.submit(task, result);
-        }
-    }
-
-    @NonNull
-    @Override
-    public <T> Future<T> submit(Callable<T> task) {
-        if (task instanceof PriorityCallable) {
-            RunnableFuture<T> futureTask = newTaskFor(task);
-            execute(futureTask);
-            return futureTask;
-        } else {
-            return super.submit(task);
-        }
-    }
-
     @Override
     protected <T> RunnableFuture<T> newTaskFor(Runnable runnable, T value) {
-        return new PriorityRunnableFutureTask<>((PriorityRunnable) runnable, value);
+        if (runnable instanceof PriorityRunnable) {
+            return new PriorityRunnableFutureTask<>((PriorityRunnable) runnable, value);
+        } else {
+            return super.newTaskFor(runnable, value);
+        }
     }
 
     @Override
     protected <T> RunnableFuture<T> newTaskFor(Callable<T> callable) {
-        return new PriorityCallableFutureTask<>((PriorityCallable<T>) callable);
+        if (callable instanceof PriorityCallable) {
+            return new PriorityCallableFutureTask<>((PriorityCallable<T>) callable);
+        } else {
+            return super.newTaskFor(callable);
+        }
     }
 }
