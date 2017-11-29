68c03b10e0171478d8a919354d2d44e4aff14a72
vitali <vitalibof@gmail.com>
2017-11-29 14:12:36 +0300

    Text can be added to the image

--- app/src/main/java/com/bov/vitali/training/common/utils/BitmapUtils.java
+++ app/src/main/java/com/bov/vitali/training/common/utils/BitmapUtils.java
@@ -1,11 +1,31 @@
 package com.bov.vitali.training.common.utils;
 
 import android.content.Context;
+import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
 import android.net.Uri;
+import android.util.DisplayMetrics;
 
 public class BitmapUtils {
+
+    public static Bitmap decodeSampledBitmapFromResource(Context context, Uri uri) {
+        try {
+            BitmapFactory.Options options = new BitmapFactory.Options();
+            options.inJustDecodeBounds = true;
+            BitmapFactory.decodeStream(context.getContentResolver().openInputStream(uri), null, options);
+            options.inJustDecodeBounds = false;
+            options.inPreferQualityOverSpeed = true;
+            return BitmapFactory.decodeStream(context.getContentResolver().openInputStream(uri), null, options);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 
     public static Bitmap decodeSampledBitmapFromResource(Context context, Uri uri, int reqWidth, int reqHeight) {
         try {
@@ -34,5 +54,48 @@ public class BitmapUtils {
             }
         }
         return size;
+    }
+
+    public static Bitmap scaleBitmap(Context context, Bitmap bitmap) {
+        DisplayMetrics metrics = context.getResources().getDisplayMetrics();
+        int displayHeight = metrics.heightPixels;
+        int displayWidth = metrics.widthPixels;
+        float ratioHeight = displayHeight / (float) bitmap.getHeight();
+        float ratioWidth = displayWidth / (float) bitmap.getWidth();
+        if (ratioHeight > ratioWidth) {
+            return scaleToFitHeight(bitmap, displayHeight);
+        } else {
+            return scaleToFitWidth(bitmap, displayWidth);
+        }
+    }
+
+    private static Bitmap scaleToFitHeight(Bitmap bitmap, int height) {
+        float factor = height / (float) bitmap.getHeight();
+        return Bitmap.createScaledBitmap(bitmap, (int) (bitmap.getWidth() * factor), height, true);
+    }
+
+    private static Bitmap scaleToFitWidth(Bitmap bitmap, int width) {
+        float factor = width / (float) bitmap.getWidth();
+        return Bitmap.createScaledBitmap(bitmap, width, (int) (bitmap.getHeight() * factor), true);
+    }
+
+    public static Bitmap drawTextToBitmap(Context context, Bitmap bitmap, String text) {
+        Resources resources = context.getResources();
+        float scale = resources.getDisplayMetrics().density;
+        Bitmap.Config bitmapConfig = bitmap.getConfig();
+        if (bitmapConfig == null) {
+            bitmapConfig = Bitmap.Config.ARGB_8888;
+        }
+        bitmap = bitmap.copy(bitmapConfig, true);
+        Canvas canvas = new Canvas(bitmap);
+        Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        paint.setColor(Color.WHITE);
+        paint.setTextSize((int) (80 * scale));
+        Rect bounds = new Rect();
+        paint.getTextBounds(text, 0, text.length(), bounds);
+        int x = (bitmap.getWidth() - bounds.width()) - PixelUtils.dp2px(context, 15);
+        int y = (bitmap.getHeight() - PixelUtils.dp2px(context, 65 ));
+        canvas.drawText(text, x, y, paint);
+        return bitmap;
     }
 }
--- app/src/main/java/com/bov/vitali/training/presentation/main/fragment/ImageChangeFragment.java
+++ app/src/main/java/com/bov/vitali/training/presentation/main/fragment/ImageChangeFragment.java
@@ -2,11 +2,13 @@ package com.bov.vitali.training.presentation.main.fragment;
 
 import android.graphics.Bitmap;
 import android.net.Uri;
+import android.widget.EditText;
 import android.widget.ImageView;
 
 import com.arellomobile.mvp.presenter.InjectPresenter;
 import com.arellomobile.mvp.presenter.ProvidePresenter;
 import com.bov.vitali.training.R;
+import com.bov.vitali.training.common.utils.BitmapUtils;
 import com.bov.vitali.training.presentation.base.fragment.BaseFragment;
 import com.bov.vitali.training.presentation.main.presenter.ImageChangePresenter;
 import com.bov.vitali.training.presentation.main.view.ImageChangeContract;
@@ -14,9 +16,10 @@ import com.bov.vitali.training.presentation.navigation.BackButtonListener;
 import com.bov.vitali.training.presentation.navigation.RouterProvider;
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.target.SimpleTarget;
-import com.bumptech.glide.request.transition.Transition;;
+import com.bumptech.glide.request.transition.Transition;
 
 import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EFragment;
 import org.androidannotations.annotations.FragmentArg;
 import org.androidannotations.annotations.ViewById;
@@ -27,6 +30,8 @@ public class ImageChangeFragment extends BaseFragment<ImageChangePresenter, Imag
     @InjectPresenter ImageChangePresenter presenter;
     @FragmentArg Uri uri;
     @ViewById ImageView ivTextToBitmap;
+    @ViewById EditText etTextToBitmap;
+    private Bitmap bitmap;
 
     @ProvidePresenter
     ImageChangePresenter provideImageChangePresenter() {
@@ -35,13 +40,34 @@ public class ImageChangeFragment extends BaseFragment<ImageChangePresenter, Imag
 
     @AfterViews
     public void afterViews() {
+        scaleBitmap();
         onBind();
     }
 
-    public void onBind() {
+    @Click(R.id.btnAddTextToBitmap)
+    public void onClick() {
+        String text = etTextToBitmap.getText().toString();
+        Bitmap changedBitmap = BitmapUtils.drawTextToBitmap(getContext(), bitmap, text);
         Glide.with(getContext())
                 .asBitmap()
-                .load(uri)
+                .load(changedBitmap)
+                .into(new SimpleTarget<Bitmap>() {
+                    @Override
+                    public void onResourceReady(Bitmap resource, Transition<? super Bitmap> transition) {
+                        ivTextToBitmap.setImageBitmap(resource);
+                    }
+                });
+    }
+
+    private void scaleBitmap() {
+        bitmap = BitmapUtils.decodeSampledBitmapFromResource(getContext(), uri);
+        bitmap = BitmapUtils.scaleBitmap(getContext(), bitmap);
+    }
+
+    private void onBind() {
+        Glide.with(getContext())
+                .asBitmap()
+                .load(bitmap)
                 .into(new SimpleTarget<Bitmap>() {
                     @Override
                     public void onResourceReady(Bitmap resource, Transition<? super Bitmap> transition) {
